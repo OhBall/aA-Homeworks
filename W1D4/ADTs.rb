9852e6ad65ea0fe2b9d87f597214dcead2fa4c5b
Oliver Ball <olivermartinball@gmail.com>
2018-05-24 21:35:59 -0400

    Fixed remove to from FILO to FIFO. Added basic tests. Removed "self" from self.stack occurrences.

--- W1D4/ADTs.rb
+++ W1D4/ADTs.rb
@@ -1,24 +1,40 @@
 class Stack
 
-
-
   def initialize
     @stack = []
   end
 
   def add(el)
-    self.stack << el
+    stack << el
+    el
   end
 
   def remove
-    self.stack.shift
+    stack.pop
   end
 
   def show
-    self.stack.dup
+    stack.dup
+  end
+
+  def display
+    stack.each { |el| print "#{el} " }
+    print "\n"
   end
 
   private
   attr_accessor :stack
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  a = Stack.new
+  a.display
+  a.add(5)
+  a.add(8)
+  a.add(10)
+  a.display
+  puts a.remove
+  a.display
+  p a.show
 end
