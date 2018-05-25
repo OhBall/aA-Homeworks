b195534a8e7106500669f4c5c2f28e7fc7fa3110
Oliver Ball <olivermartinball@gmail.com>
2018-05-24 21:43:30 -0400

    Added tests

--- W1D4/queue.rb
+++ W1D4/queue.rb
@@ -21,3 +21,13 @@ class Queue
   attr_accessor :queue
 
 end
+
+if __FILE__ == $PROGRAM_NAME
+  a = Queue.new
+  (0..7).to_a.each {|x| a.enqueue(x)}
+  p a.show
+  puts "Dequeued element: #{a.deqeueue}"
+  puts "Dequeued element: #{a.deqeueue}"
+  puts "Dequeued element: #{a.deqeueue}"
+  puts "Queue.show output: #{a.show}"
+end
