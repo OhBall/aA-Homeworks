325913847a1130362d8ecbccebd02c52e9542482
Oliver Ball <olivermartinball@gmail.com>
2018-06-06 22:33:15 -0400

    add better drivers query

--- W3D4/n+1_buster/app/models/route.rb
+++ W3D4/n+1_buster/app/models/route.rb
@@ -30,6 +30,16 @@ class Route < ApplicationRecord
   end
 
   def better_drivers_query
-    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+    all_drivers
   end
 end
