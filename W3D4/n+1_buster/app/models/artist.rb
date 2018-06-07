bdc3a331a70b6fd29ab50c3102ca589898b08365
Oliver Ball <olivermartinball@gmail.com>
2018-06-06 21:57:55 -0400

    Initial commit for n+1 problems

---
+++ W3D4/n+1_buster/app/models/artist.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    albums = self.albums.includes(tracks)
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+  end
+end
---
+++ W3D4/n+1_buster/app/models/route.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id         :bigint(8)        not null, primary key
+#  number     :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Route < ApplicationRecord
+  has_many :buses,
+    class_name: 'Bus',
+    foreign_key: :route_id,
+    primary_key: :id
+
+  def n_plus_one_drivers
+    buses = self.buses
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+
+  def better_drivers_query
+    # TODO: your code here
+  end
+end
