782bc76a190a9c98a4ee9727c0ee6ae48fe1d2e4
Oliver Ball <olivermartinball@gmail.com>
2018-06-13 00:03:45 -0400

    last commit. Issues with find_by_credentials

--- W4D3/auth_hw/app/models/user.rb
+++ W4D3/auth_hw/app/models/user.rb
@@ -1,2 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def find_by_credentials(username, password)
+    user = User.where(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+  end
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token
+    self.session_token = generate_session_token
+    self.save
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
