766667ff202c9f4b364395703811475a5b93ff70
Oliver Ball <olivermartinball@gmail.com>
2018-06-14 22:21:38 -0400

    changed gemfile and bundle installed

--- W4D5/music_app/config/environments/test.rb
+++ W4D5/music_app/config/environments/test.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Configure default mail server
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
