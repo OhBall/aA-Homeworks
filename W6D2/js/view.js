8e715acf62ec3f501e2a5a7106453f544f3db302
Oliver Ball <olivermartinball@gmail.com>
2018-06-25 23:08:15 -0400

    complete phase 4

--- W6D2/js/view.js
+++ W6D2/js/view.js
@@ -23,6 +23,7 @@ View.prototype.exercise1 = function () {
   //Result: Every square should turn orange (we already have a CSS rule)
 
   //your code here!
+  $('li').addClass('orange');
 };
 
 View.prototype.exercise2 = function () {
@@ -30,6 +31,7 @@ View.prototype.exercise2 = function () {
   //Result: Every square vanishes
 
   //your code here!
+  $('li').remove();
 };
 
 View.prototype.exercise3 = function () {
@@ -37,6 +39,9 @@ View.prototype.exercise3 = function () {
   //Result: An <h1> with the text 'i love jquery' appears under the grid.
 
   //your code here!
+  const heart = $("<h1>");
+  heart.text("i love jquery");
+  $("#easel").append(heart);
 };
 
 View.prototype.exercise4 = function () {
@@ -44,6 +49,15 @@ View.prototype.exercise4 = function () {
   //Result: Your name appears in every other square.
 
   //your code here!
+  const squares = $("li");
+  function addName(index, el) {
+    if (index % 2 === 0) {
+      el = $(el);
+      el.text("oliver");
+    }
+  }
+  $.each(squares, addName);
+
 };
 
 View.prototype.exercise5 = function () {
