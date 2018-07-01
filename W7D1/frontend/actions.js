59cd8211395eb141c1859536f346a21449a77724
Oliver Ball <olivermartinball@gmail.com>
2018-07-01 19:49:03 -0400

    finish commit

---
+++ W7D1/frontend/actions.js
@@ -0,0 +1,10 @@
+function selectLocation(city, jobs) {
+  return {
+    type: "SWITCH_LOCATION",
+    city: city,
+    jobs: jobs
+  };
+}
+
+
+export default selectLocation;
--- W7D1/frontend/reducer.js
+++ W7D1/frontend/reducer.js
@@ -1,10 +1,20 @@
+import merge from 'lodash/merge';
+
 const initialState = {
-  city: "Please Select", 
+  city: "Please Select",
   jobs: []
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch(action.type) {
+    case "SWITCH_LOCATION":
+      return {
+        city: action.city,
+        jobs: action.jobs
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
